c6ae6f7127d6d6a3b13e76809704e997753d5437
seshin <sam@baremetalnetworks.com>
2015-06-28 17:27:40 +0100

    init

--- highway.rb
+++ highway.rb
@@ -1,1 +1,30 @@
-p "hello from highway.rb"
+#!/usr/bin/env ruby
+require 'json'
+
+opt_parser = OptionParser.new do |opts|
+  exec_name = File.basename(PROGRAM_NAME)
+  opts.banner = "###### BMN IMS ######## \n # BareMetal's Infrastructure Management Console\n
+  Usage: #{exec_name} <optional host file> \n
+
+  Will load hosts.json from current directory or /etc/superhighway/hosts.json must be valid JSON"
+end
+
+opt_parser.parse!
+
+hosts = ARGV.shift || '/etc/superhighway/hosts.json'
+
+if hosts && File.exists? hosts
+  main(hosts)
+=begin
+else
+  STDERR.puts "Error: Couldn't find an infrastructure.hosts YAML file, creating one now in /tmp"
+  `touch /tmp/hosts.json`
+  p 'restart the REPL'
+  exit 1
+=end
+end
+
+def main(hosts_file)
+  servers = JSON.parse(hosts_file)
+80
+end
