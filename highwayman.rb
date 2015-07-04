f15419ea981c808def2e45a898366031f11991ae
seshin <sam@baremetalnetworks.com>
2015-07-04 07:38:47 +0100

    swapping datastructs

--- highwayman.rb
+++ highwayman.rb
@@ -5,7 +5,10 @@ require 'readline'
 require 'highline'
 require 'net/ssh'
 require 'fuzzy_match'
+# Author: SJK, Senior Developer, BareMetalNetworks Corp
+# First week of July 2015
 
+## Fuzzymatch.new(['foo', 'bar', 'baz']).find("far")
 all_srv = Array.new
 all_srv = %w{10.0.1.200 10.0.1.32 10.0.1.27 10.0.1.10 10.0.1.7 10.0.1.19 10.0.1.20 10.0.1.21 10.0.1.22 10.0.1.28}
 
@@ -14,7 +17,8 @@ srvs = {:datastore0 => '10.0.1.18', :datastore2 => '10.0.1.32', :app2 => '10.0.1
 
 opt_parser = OptionParser.new do |opts|
   exec_name = File.basename($PROGRAM_NAME)
-  opts.banner = "###### Highwayman IMS ######## \n # BareMetal's Infrastructure Management Console\n
+  opts.banner = "###### Highway IMS ######## \n # BareMetal's Infrastructure Management Console\n
+  # GNU Readline supported ctr-X and Alt-X emacs keybindings available\n
   Usage: #{exec_name} <optional host file> \n
 
   Will load hosts.json from current directory or /etc/superhighway/hosts.json must be valid JSON"
@@ -29,19 +33,19 @@ $XGUI = ARGV.shift || false
 # extended prompt yes||No
 $EXTENDPROMPT = true
 
-if hosts && File.exists?(hosts)
-  main(hosts)
-
-  # TODO finish this
-#elsif #parser
- # `git clone https://github.com/baremetalnetworks/clusterforge`
-
-else
-  STDERR.puts "Error: Couldn't find an infrastructure.hosts YAML file, creating one now in /tmp from /etc/hosts"
-  `touch /tmp/hosts.json`
-  p 'restart the REPL'
-  #exit 1
-end
+# if hosts && File.exists?(hosts)
+#   main(hosts)
+#
+#   # TODO finish this
+# #elsif #parser
+#  # `git clone https://github.com/baremetalnetworks/clusterforge`
+#
+# else
+#   STDERR.puts "Error: Couldn't find an infrastructure.hosts YAML file, creating one now in /tmp from /etc/hosts"
+#   `touch /tmp/hosts.json`
+#   p 'restart the REPL'
+#   #exit 1
+# end
 
 $stack = Array.new
 
@@ -66,7 +70,7 @@ end
       Net::SSH.start(@host, @user, :password => @password) do |ssh|
         ssh.open_channel do |channel|
           channel.exec command do |channel, success|
-            abort "Could not execute #{command}" unless success
+            raise "Could not execute #{command}" unless success
             channel.on_data do |channel, data|
               return data
 
@@ -88,15 +92,23 @@ $history = Array.new # for history cache
 ## Dispatch table for builtin commands IMS> batch <command>  IMS> host :staged0 <command>
 ## Buildout options table
 ## Create another class to Manage those nodes?
+## Output to a file or stuff in a database options
 ###########################
 
 def main(srvs)
   command = nil
   cmd_count = 0
   conns = Array.new
-  threadedcmd = lambda { |conn| Thread.new { p "### Host #{conn.host} ###\n" + conn.open_ssh_conn(command) }}
+  nodeRes = Hash.new([])
+
+  # Function issuers, threaded and non-threaded
+  threadedcmd = lambda { |conn| Thread.new { nodeRes[conn.host].push(conn.open_ssh_conn(command)) }}
+  #threadedcmd = lambda { |conn| Thread.new { p "### Host #{conn.host} ###\n" + conn.open_ssh_conn(command) }}
   issuecmd = lambda { |conn|  p "### Host #{conn.host} ###\n" + conn.open_ssh_conn(command) }
+
+  # Construct node objects, populate them with hash: keys are hostnames and value is array for results
   srvs.each {|srv| conns.push(NodeManager.new(srv, 'vishnu', 'password' ))}
+  conns.each {|conn| h = conn.open_ssh_conn('hostname'); nodeRes[h.chomp.to_s.to_sym] = ['alive']  }
 
   begin
     while command != ('exit' || 'quit')
@@ -112,6 +124,7 @@ def main(srvs)
         p "[Issuer] Error: #{err.inspect}"
         next
       end
+      p nodeRes
     end
 
   rescue => err
