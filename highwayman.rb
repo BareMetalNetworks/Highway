9b0a0133884bcba290d292581febab536e047e1e
seshin <sam@baremetalnetworks.com>
2015-07-05 07:56:30 +0100

    added option handling 90% done

--- highwayman.rb
+++ highwayman.rb
@@ -6,55 +6,80 @@ require 'highline'
 require 'net/ssh'
 require 'fuzzy_match'
 require 'redis'
+$PROGRAM_NAME = 'IMS.rb'
+$VERSION = '0.1.0'
+###########################################################################################
+# Author: SJK, Senior Developer, BareMetalNetworks.com                                    #
+# First week of July 2015                                                                 #
+##### Future Features #####################################################################
+## Populate hosts hostname by calling each ip and running hostname during the setup       #
+## Have both batch and single host command issue                                          #
+## Write a parser/lexxer for builtins                                                     #
+## History support -- redis                                                               #
+## Refactor Node into Node                                                                #
+## Dispatch table - builtin commands IMS> batch <command>  IMS> host :staged0 <command>   #
+## Buildout options table                                                                 #
+## Create another class to Manage those nodes?                                            #
+## Output to a file or stuff in a database options -- redis                               #
+## Batch command execution and cluster management REPL and plain vanilla CLI toolkit      #
+## Redis backed store of hostname/node info plus node statistics & command issue returns  #
+## Also redis backed store for fuzzymatch future.                                         #
+###########################################################################################
 
-##### Future Features #####
-## Populate hosts hostname by calling each ip and running hostname during the setup
-## Have both batch and single host command issue
-## Write a parser/lexxer for builtins
-## History support -- redis
-## Refactor Node into Node
-## Dispatch table for builtin commands IMS> batch <command>  IMS> host :staged0 <command>
-## Buildout options table
-## Create another class to Manage those nodes?
-## Output to a file or stuff in a database options -- redis
-## Batch command execution and cluster management REPL and plain vanilla CLI toolkit
-## Redis backed store of hostname/node info plus node statistics & command issue returns
-## Also redis backed store for fuzzymatch future.
-
-# Author: SJK, Senior Developer, BareMetalNetworks Corp
-# First week of July 2015
-
+## History match/Command completion
 ## Fuzzymatch.new(['foo', 'bar', 'baz']).find("far")
 
-all_srv = Array.new
 
-def setup_redis(host, port, dbfixnum)
-  redis = Redis.new({:host => host, :port => port})
-  redis.select(dbfixnum)
-  redis
-end
 
 ## Needs command parsing
-## DB0 is for sysop's system events, DB1 is for IMS, DB2,3 reserved but unallocated, DB4 is for testing/junk
+## Stack0 is redis only server on 10.0.1.17
+# On stack0 DB0 is for sysop's system events, DB1 is for IMS, DB2,3 reserved but unallocated,
+# DB4 is for testing/junk
+
+
+options = {}
+opt_parser = OptionParser.new do |opts|
+  exec_name = File.basename($PROGRAM_NAME)
+  opts.banner = "###### Highway IMS ######## \n # BareMetal's Infrastructure Management Console\n
+  # GNU Readline supported Ctrl-* and Alt-* emacs keybindings available\n
+  Usage: #{exec_name} <options> \n""   "
+
+  options[:version] = false
+  opts.on('-v', '--verbose', 'Wordy output') { options[:verbose] = true}
+  options[:logfile] = nil
+  opts.on('-l', '--logfile FILE', 'Write STDOUT to a file') { |f| options[:logfile] = f }
+
+  options[:redishost] = nil
+  opts.on('-h', '--host REDIS-HOST', 'Redis database host, required for command completion, history, and pushing
+events out to an alt interface (Like a web UI or a remote syslog srv). Defaults to localhost') { |h|
+    options[:redishost] = h if  }
+
+  options[:redisport] = nil
+  opts.on('-p', '--port REDIS-PORT', 'Redis database host port') { |p| options[:redisport] = p }
+  options[:redistable] = nil
+  opts.on('-t', '--redis-table REDIS-DATABASE-TABLE', 'Redis database table number, e.g. 1 or 3') { |d|
+    options[:redistable] = d }
+
+
+
+
+end ; opt_parser.parse!
+
+def setup_redis
+  redis = Redis.new({:host => options[:redishost], :port => options[:redisport]})
+  redis.select(options[:redistable])
+  redis
+end
 redi = setup_redis('10.0.1.17', '6379', 1)
 
-File.load('/etc/hosts') || File.load('/etc/cluster', 'r')
+redi.key(:node_ips)?
 
+all_srv = Array.new
 all_srv = %w{10.0.1.200 10.0.1.32 10.0.1.27 10.0.1.10 10.0.1.7 10.0.1.19 10.0.1.20 10.0.1.21 10.0.1.22 10.0.1.28}
 
 srvs = {:datastore0 => '10.0.1.18', :datastore2 => '10.0.1.32', :app2 => '10.0.1.27', :app3 => '10.0.1.28', :app1 => "10
 .0..23"}
 
-opt_parser = OptionParser.new do |opts|
-  exec_name = File.basename($PROGRAM_NAME)
-  opts.banner = "###### Highway IMS ######## \n # BareMetal's Infrastructure Management Console\n
-  # GNU Readline supported ctr-X and Alt-X emacs keybindings available\n
-  Usage: #{exec_name} <optional host file> \n
-
-  Will load hosts.json from current directory or /etc/superhighway/hosts.json must be valid JSON"
-end
-
-opt_parser.parse!
 
 hosts = ARGV.shift || '/etc/superhighway/hosts.json'
 password = ARGV.shift || false
